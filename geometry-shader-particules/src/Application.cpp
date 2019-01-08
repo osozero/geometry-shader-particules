fae67d8528175d84059d5cfaeb00eaacf1b11f17
osozero <sametozer13@gmail.com>
2019-01-07 19:24:30 -0800

    simple geometry shader implementated.

--- geometry-shader-particules/src/Application.cpp
+++ geometry-shader-particules/src/Application.cpp
@@ -15,11 +15,26 @@ const char *vertexShaderSource = "#version 330 core\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y,0.0, 1.0);\n"
 "}\0";
+
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
 "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
+
+const char *geometryShaderSource = "#version 330 core\n"
+"layout (points) in;\n"
+"layout (line_strip,max_vertices =5) out;\n"
+"void main()\n"
+"{\n"
+"   gl_Position=gl_in[0].gl_Position + vec4(0.0,-0.1f,0.0,0.0);\n"
+"   EmitVertex();\n"
+"   gl_Position=gl_in[0].gl_Position + vec4(0.0,0.1f,0.0,0.0);\n"
+"   EmitVertex();\n"
+
+"   EndPrimitive();\n"
 "}\n\0";
 
 
@@ -46,14 +61,49 @@ int main()
 	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
 	glCompileShader(vertexShader);
 
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
 	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
 	glCompileShader(fragmentShader);
 
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	int geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+	glShaderSource(geometryShader, 1, &geometryShaderSource, nullptr);
+	glCompileShader(geometryShader);
+
+	glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(geometryShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::GEOMETRY::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
 	int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, geometryShader);
 	glAttachShader(shaderProgram, fragmentShader);
+
 	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
 
 
 	glDeleteShader(vertexShader);
@@ -62,36 +112,42 @@ int main()
 	float vertices[] = { -0.5f,0.5f,
 						  0.5f,0.5f };
 
+	float points[] = { 0.0f,0.2f };
+
 	unsigned int vbo, vao;
 
 	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2 , GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
+	glGenBuffers(1, &vbo);
+
+
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 1 , GL_FLOAT, GL_FALSE, 1*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	
-
 	while(!glfwWindowShouldClose(window))
 	{
 			glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			glViewport(0, 0, WIDTH, HEIGHT);
-
+			//glViewport(0, 0, WIDTH, HEIGHT);
 			glUseProgram(shaderProgram);
 			glBindVertexArray(vao);
 
-			glDrawArrays(GL_LINE, 0, 2);
+			glDrawArrays(GL_POINTS, 0, 1);
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 	}
-	
+
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
 
 	glfwTerminate();
 
@@ -99,3 +155,9 @@ int main()
 }
 
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
