ecf4bdc4afc5c9bc266c0944582a58d15eef3e96
osozero <sametozer13@gmail.com>
2019-01-07 18:36:27 -0800

    initialized

---
+++ geometry-shader-particules/src/Application.cpp
@@ -0,0 +1,90 @@
+#define GLEW_STATIC
+
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <ostream>
+#include <iostream>
+
+const unsigned int WIDTH = 1800;
+const unsigned int HEIGHT = 1200;
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec2 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, 0, 1.0);\n"
+"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
+
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, 1200, "GeometryShader", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	int fragmentShader;
+
+	float vertices[] = { -0.5f,0.5f,
+					  0.5f,0.5f };
+
+	unsigned int vbo, vao;
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2 * sizeof(float), GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	
+
+	while(true)
+	{
+		if (!glfwWindowShouldClose(window))
+		{
+
+			glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glViewport(0, 0, WIDTH, HEIGHT);
+
+			//glDrawArrays(GL_LINE, 0, 2);
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+	}
+	
+
+	glfwTerminate();
+
+	return 0;
+}
